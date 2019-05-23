4e291d29aac4cc31fde68f99121b393047b93693
pchelkanat <pchelkanat@mail.ru>
2019-05-23 16:55:53 +0300

    Добавлено быстрое возведение в степень по модулю.

--- prog/utils.py
+++ prog/utils.py
@@ -15,6 +15,19 @@ def n_to_s_d(n):
     return power, d
 
 
+def powmod(a, x, p):
+    x_bin = list(bin(x)[2:])
+    a, p = np.uint32(a), np.uint32(p)
+    # print(type(a), type(p))
+    mod = a
+    for i in range(1, len(x_bin), 1):
+        if int(x_bin[i]) == 0:
+            mod = mod ** 2 % p
+        elif int(x_bin[i]) == 1:
+            mod = a * mod ** 2 % p
+    return mod
+
+
 # проверка нечетного n на простые неповторяющиеся множители
 def check_n(n):
     arr = np.array(factorization(n))  # возвращает (число, его степень)
@@ -63,3 +76,4 @@ def combinations(mylist, t):
     return np.array(list(itertools.combinations(mylist, t)), dtype=np.int32)
 
 # if __name__ == "__main__":
+# print(powmod(5, 97651 - 1, 97651))
