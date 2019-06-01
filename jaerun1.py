7391172fb437538ae806fa1e0bc8561fe7ce4032
pchelkanat <pchelkanat@mail.ru>
2019-06-02 01:12:27 +0300

    update code to compute for t>2

--- jaerun1.py
+++ jaerun1.py
@@ -1,4 +1,4 @@
-from jaeschke import t_2, t_more_3
+from jaeschke import t_more_3
 from utils import readfile
 
 bases = [2, 3, 5, 7, 11, 13, 17, 19,
@@ -8,5 +8,4 @@ primes = readfile("primes/primes_1m.txt")
 
 if __name__ == "__main__":
     print(2, 6)
-    t_more_3(bases[:2], 10 ** 6, primes)
-
+    t_more_3(bases[:2], 10 ** 6, 3, primes)
