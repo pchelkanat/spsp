57fdf4b1185edc499802656097f1bed12059397b
pchelkanat <pchelkanat@mail.ru>
2019-06-01 16:46:11 +0300

    jaerun add

---
+++ jaerun1.py
@@ -0,0 +1,14 @@
+from jaeschke import t_2
+from utils import readfile
+
+bases = [2, 3, 5, 7, 11, 13, 17, 19,
+         23, 29, 31, 37, 41, 43, 47, 53]
+
+primes = readfile("primes/primes_1m.txt")
+
+if __name__ == "__main__":
+    for i in range(6, 10, 2):
+        for j in range(2, 5):
+            print(i, j)
+            t_2(bases[:j], 10 ** i, primes)
+
